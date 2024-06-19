f68ad12c5f26c941614cf2697728840252eb99b0
liding <68780491@qq.com>
2024-06-19 22:28:06 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');  
+  
+// A simple testing function
+function testAddition() {  
+  assert.strictEqual(1 + 1, 2, '1 + 1 should equal 2');  
+}  
+
+// Run tests 
+testAddition();
